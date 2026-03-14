b2ba7f194499a7b86d3c5fce1b9eab210a62a366
sanjaikumar2006 <sbsanjaikumar@gmail.com>
2026-03-14 14:00:23 +0530

    🔧 Prep for Vercel deployment

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+if (!MONGODB_URI && process.env.NODE_ENV === 'production') {
+  throw new Error('Please define the MONGODB_URI environment variable inside Vercel Dashboard');
 }
 
 let cached = (global as any).mongoose;
